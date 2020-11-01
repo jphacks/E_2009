b2600a7ed458de3787eb55742ea58054de55e056
y-yana <66319933+y-yana@users.noreply.github.com>
2020-11-01 16:46:33 +0900

    templates 追加

---
+++ templates/form.php
@@ -0,0 +1,4 @@
+<?php $title = 'Kindred form'; ?>
+<?php include('./header.php'); ?>
+<!-- ここにbodyの中身を記述 -->
+<?php include('./footer.php'); ?>
---
+++ templates/list.php
@@ -0,0 +1,4 @@
+<?php $title = 'Kindred university'; ?>
+<?php include('./header.php'); ?>
+<!-- ここにbodyの中身を記述 -->
+<?php include('./footer.php'); ?>
