db11a4126bb20bef64d4ba0da312d65eba8451b4
y-yana <66319933+y-yana@users.noreply.github.com>
2020-11-03 02:25:15 +0900

    viewport追加

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1">
     <link rel="stylesheet" type="text/css" href="../static/style/css/style.min.css"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
     <title><?php echo $title ?></title>
