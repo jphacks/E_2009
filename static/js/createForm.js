0210a92505c338f7118e209fafa4ca0a8444d00d
y-yana <66319933+y-yana@users.noreply.github.com>
2020-11-02 19:45:52 +0900

    活動日時の制御成功

--- static/js/createForm.js
+++ static/js/createForm.js
@@ -6,7 +6,7 @@ window.onload = function () {
   for (var i = 0; i < 7; i++){
 
     // 月～日までのチェックボックスをそれぞれ生成
-    document.getElementById("activityDate").insertAdjacentHTML('beforeend', '<div id="selectTime"><span><input type="checkbox">' + date[i] + '曜日</span><span><select name="startTime' + i + '" id="start' + i + '"></select>～<select name="endTime' + i + '" id="end' + i + '"></select></span></div>');
+    document.getElementById("activityDate").insertAdjacentHTML('beforeend', '<div id="selectTime"><span><input type="checkbox" name="date" onChange="activityDateCheck();">' + date[i] + '曜日</span><span><select name="startTime' + i + '" id="start' + i + '" disabled></select>～<select name="endTime' + i + '" id="end' + i + '" disabled></select></span></div>');
 
     // 24時間分のドロップダウンリストを生成
     for (var j = 0; j < 25; j++){
@@ -19,6 +19,25 @@ window.onload = function () {
 
       document.getElementById("start" + i).appendChild(startOption);
       document.getElementById("end" + i).appendChild(endOption);
+    }
+
+  }
+
+}
+
+// チェックボックスの状態判定
+function activityDateCheck() {
+
+    const elements = document.getElementsByName("date");
+
+  for (let i = 0; i < elements.length; i++){
+
+    if (elements[i].checked) {
+      document.getElementById("start" + i).disabled = false;
+      document.getElementById("end" + i).disabled = false;
+    } else {
+      document.getElementById("start" + i).disabled = true;
+      document.getElementById("end" + i).disabled = true;
     }
 
   }
