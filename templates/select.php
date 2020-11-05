cfeaa9b8c0c6ec25c4c2755ae04af912d17d0656
y-yana <66319933+y-yana@users.noreply.github.com>
2020-11-05 14:11:27 +0900

    学部名とキャンパス名の表示に成功

--- templates/select.php
+++ templates/select.php
@@ -6,6 +6,23 @@
 
 <div id="selectArea">
 <!-- PHPで大学名とキャンパス名をDBから取得してここに出力する -->
+<?php
+
+  // 変数にSELECT文を格納
+  $sql = "SELECT SCHOOL_NAME,DEPART_NAME,CAMPUS_NAME FROM SCHOOL WHERE SCHOOL_NAME='".$_SESSION['schoolName']."'";
+
+  // SELECT文の実行
+  $schoolSearch = $mysqli->query($sql);
+
+  // 大学名の出力
+  echo '<p>'.$_SESSION['schoolName'].'</p>';
+
+  // 学部名とキャンパス名を出力
+  foreach ($schoolSearch as $row){
+    echo '<p>'.$row['DEPART_NAME'].$row['CAMPUS_NAME'].'</p>';
+  }
+
+?>
 </div>
 
 <button onclick="location.href='./top.php'"><span class="material-icons">arrow_back</span></button>
